8f4b2540b7509b7bf6762e7228740483168c01ed
platik777 <ps7654400@icloud.com>
2022-09-20 06:27:57 +0300

    Final parsing v2.0

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
     std::vector <std::string> FileNames;
     std::vector <std::string> FileCommands;
 
-    for ( int i = 1; i < argc; i++ ) {
+    for (int i = 1; i < argc; i++) {
         std::string str = argv[i];
         if ( str.find('.') < str.length() ) {
             FileNames.push_back(str);
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    for ( int j = 0; j < FileCommands.size(); j++ ) {
+    for (int j = 0; j < FileCommands.size(); j++) {
         if ( FileCommands[j].length() > 2 && FileCommands[j].length() < 6 ) {
             for (int i = 1; i < FileCommands[j].length(); i++ ) {
                 char str[] = {'-', FileCommands[j][i], '\0'};
@@ -26,50 +26,50 @@ int main(int argc, char* argv[]) {
     }
 
     int count = 0;
-    for ( int j = 0; j < FileNames.size(); j++ )
+    for (int j = 0; j < FileNames.size(); j++)
     {
-        std::ifstream file_in( FileNames[j] );
-        for ( auto & FileCommand : FileCommands )
+        std::ifstream file_in(FileNames[j]);
+        for (int i = 0; i < FileCommands.size(); i++)
         {
-            if ( FileCommand == "-l"  || FileCommand == "--lines" ) {
+            if (FileCommands[i] == "-l"  || FileCommands[i] == "--lines") {
                 std::string str;
-                while ( std::getline( file_in, str ) ) {
+                while (std::getline(file_in, str)) {
                     count++;
                 } std::cout << "Count of line in " << j + 1 << " file: " << count << std::endl;
                 count = 0;
                 file_in.clear();
                 file_in.seekg(0);
             }
-            else if ( FileCommand == "-c" || FileCommand == "--bytes" ) {
+            else if (FileCommands[i] == "-c" || FileCommands[i] == "--bytes") {
                 file_in.seekg(0, std::ios ::end);
-                int size_file = file_in.tellg();
-                file_in.seekg( 0, std::ios ::beg );
+                long long size_file = file_in.tellg();
+                file_in.seekg(0, std::ios ::beg);
                 std::cout << "Bytes in " << j + 1 << " file: " << size_file << std::endl;
                 file_in.clear();
                 file_in.seekg(0);
             }
-            else if ( FileCommand == "-w" || FileCommand == "--words" ) {
+            else if (FileCommands[i] == "-w" || FileCommands[i] == "--words") {
                 std::string word;
-                while( file_in >> word ){
+                while(file_in >> word){
                     count++;
                 } std::cout << "Words in " << j + 1 << " file: " << count << std::endl;
                 count = 0;
                 file_in.clear();
                 file_in.seekg(0);
             }
-            else if ( FileCommand == "-m" || FileCommand == "--chars" ) {
+            else if (FileCommands[i] == "-m" || FileCommands[i] == "--chars") {
                 char c;
-                while( file_in.get(c) ){
-                    if( std::isalpha(c) ){
+                while(file_in.get(c)){
+                    if(std::isalpha(c)){
                         count++;
                     }
-                } std::cout << "Letters in " << j + 1 << " file: " << count << '\n' << '\n';
+                } std::cout << "Letters in " << j + 1 << " file: " << count << std::endl;
                 count = 0;
                 file_in.clear();
                 file_in.seekg(0);
             }
         }
-        file_in.close();
+        std::cout << '\n';
     }
     system("pause");
     return 0;
